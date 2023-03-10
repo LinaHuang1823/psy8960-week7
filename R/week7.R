2d58af5978c0bca8bef628c0b998db0e821b39bc
Lina Huang <huan2384@umn.edu>
2023-03-10 11:51:53 -0600

    Data Import and Cleaning

--- R/week7.R
+++ R/week7.R
@@ -1,8 +1,17 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-
+library(tidyverse)
+library(lubridate)
 
 
 
 #Data Import and Cleaning
+week7_tbl<-read.csv("../data/Week3.csv")%>%
+  mutate(timeStart = ymd_hms(timeStart),
+        mutate(across(contains("q"), as.integer)))%>%
+  mutate(timeEnd = ymd_hms(timeEnd))%>%
+  mutate(condition = recode(condition, "A" = "Block A", "B" = "Block B", "C" = "Control"),
+         gender = recode(gender, "M" = "Male", "F" = "Female")) %>%
+  filter(q6 == 1) %>%
+  select(-q6)%>%
+  mutate(timeSpent = as.numeric(difftime(timeEnd, timeStart, units = "mins")))
