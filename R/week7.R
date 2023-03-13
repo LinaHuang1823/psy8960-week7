8416b6df9bc691736209f60dc75c1bd37b990ed7
Lina Huang <huan2384@umn.edu>
2023-03-12 23:59:38 -0500

    update 4

--- R/week7.R
+++ R/week7.R
@@ -9,8 +9,8 @@ library(GGally)
 week7_tbl<-read.csv("../data/Week3.csv")%>%
   mutate(timeStart = ymd_hms(timeStart))%>%
   mutate(timeEnd = ymd_hms(timeEnd))%>%
-  mutate(condition = recode(condition, "A" = "Block A", "B" = "Block B", "C" = "Control"),
-         gender = recode(gender, "M" = "Male", "F" = "Female")) %>%
+  mutate(gender = factor(gender, levels = c("M","F"), labels=c("Male","Female")))%>%
+  mutate(condition = factor(condition, levels = c("A","B","C"),labels=c("Block A", "Block B", "Control")))%>%
   filter(q6 == 1) %>%
   select(-q6)%>%
   mutate(timeSpent = as.numeric(difftime(timeEnd, timeStart, units = "mins")))
@@ -22,35 +22,40 @@ week7_tbl<-read.csv("../data/Week3.csv")%>%
 week7_tbl%>%
   select(starts_with("q"))%>% 
   ggpairs()
-week7_tbl%>%
-  ggplot(aes(x = timeStart, y = q1)) +
-  geom_point() +
-  labs(x = "Date of Experiment", y = "Q1 Score")
-
-
-week7_tbl %>% 
-  mutate(gender = factor(gender, levels = c("Male", "Female"))) %>%
-  ggplot(aes(x = q1, y = q2, color = gender)) +
-  geom_point(position = "jitter") +
-  labs(x = "q1", y = "q2", color = "Participant Gender") 
-week7_tbl %>% 
-  mutate(gender = factor(gender, levels = c("Male", "Female"))) %>%
-ggplot(aes(x = q1, y = q2)) +
-  geom_point(position = "jitter") +
+(ggplot(week7_tbl,aes(x = timeStart, y = q1)) +
+  geom_point(size=0.8) +
+  labs(x = "Date of Experiment", y = "Q1 Score")+
+  theme(axis.title = element_text(size = 7),
+        axis.text = element_text(size = 5,color="#666666")))%>%
+  ggsave("../figs/fig1.png",.,width = 4.8, height = 2.8)
+(ggplot(week7_tbl,aes(x = q1, y = q2, color = gender)) +
+  geom_point(position = "jitter",size=0.95) +
+  labs(x = "q1", y = "q2", color = "Participant Gender")+
+  theme(text = element_text(size = 6.2),axis.text=element_text(color="#666666")))%>%
+  ggsave("../figs/fig2.png",.,width = 4.9, height = 2.9)
+(ggplot(week7_tbl,aes(x = q1, y = q2)) +
+  geom_point(position = "jitter",size=0.98) +
   facet_grid(. ~ gender) +
-  labs(x = "Score on Q1", y = "Score on Q2") 
-
-week7_tbl %>%
-  mutate(gender = factor(gender, levels = c("Male", "Female"))) %>%
-  ggplot(aes(x = gender, y =timeSpent)) +
+  labs(x = "Score on Q1", y = "Score on Q2")+
+  theme(text = element_text(size = 6.4),
+        axis.text=element_text(color="#666666")))%>%
+  ggsave("../figs/fig3.png",.,width = 5, height = 2.8)
+(ggplot(week7_tbl,aes(x = gender, y =timeSpent)) +
   geom_boxplot() +
-  labs(x = "Gender", y = "Time Elapsed (mins)") 
-
-week7_tbl %>%
-  ggplot(aes(x = q5, y = q7, color = condition)) +
-  geom_jitter(width=0.2)+
-  stat_smooth(method = "lm",se = FALSE)+
+  labs(x = "Gender", y = "Time Elapsed (mins)")+
+  theme(text = element_text(size = 6.5),
+        axis.text=element_text(color="#666666")))%>%
+  ggsave("../figs/fig4.png",., width = 4.8, height = 2.8)
+(ggplot(week7_tbl,aes(x = q5, y = q7, color = condition)) +
+  geom_jitter(width=0.2,size=0.95)+
+  stat_smooth(method = "lm",size=0.75,se = FALSE)+
   labs(x = "Score on Q5", y = "Score on Q7",color = "Experimental Condition")+
   theme(legend.position = "bottom",
-        legend.background = element_rect(fill = "#E0E0E0"))+
-  coord_fixed(ratio=0.5)
+        legend.background = element_rect(fill = "#E0E0E0"),
+        legend.text = element_text(size = 6),
+        legend.title = element_text(size = 7.2),
+        legend.margin = margin(3, 3, 3, 3, "pt"),
+        #legend.key.size = unit(0.8, "cm"),
+        axis.title = element_text(size = 6.8),
+        axis.text = element_text(size = 5.5,color="#666666")))%>%
+  ggsave("../figs/fig5.png",.,width = 4.5, height = 2.8,dpi = 600)
